c9b7f322ddfe28109447ac9442725e661f71919a
satooru65536 <ryuto4619@gmail.com>
2024-04-04 01:36:44 +0900

    test: storybookの設定を変更

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../**/*.stories.@(jsx|tsx|mdx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
