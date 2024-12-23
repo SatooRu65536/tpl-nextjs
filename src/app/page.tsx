892c5da28fe4dcae1f49da18b222147de826b0e5
satooru65536 <ryuto4619@gmail.com>
2024-12-24 05:10:10 +0900

    feat: Next.js のプロジェクトをセットアップ

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Button from '@/components/Button';
+import Dialog from '@/components/Dialog';
+import { useCallback, useState } from 'react';
+import { styled } from 'restyle';
+
+const Center = styled('div', {
+  'display': 'flex',
+  'justifyContent': 'center',
+  'alignItems': 'center',
+  'height': '100vh',
+  'fontSize': '2rem',
+
+  '> div': {
+    textAlign: 'center',
+  },
+});
+
+export default function Home() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDialog = useCallback(() => {
+    setOpen((prev) => !prev);
+  }, []);
+
+  return (
+    <Center>
+      <div>
+        <h1>Hello, Next.js!</h1>
+        <Button onClick={() => { toggleDialog(); }}>Click me!!</Button>
+        <Dialog onOpenChange={setOpen} open={open} />
+      </div>
+    </Center>
+  );
+}
